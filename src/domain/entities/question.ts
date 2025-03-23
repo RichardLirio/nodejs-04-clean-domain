d8b3f513f34a4f3b04c7d8bdc7cb86cebe79f7f9
Richard Lirio <richardlirio@hotmail.com>
2025-03-22 21:04:02 -0300

    Fundamentos de DDD - Mapeando relacionamentos

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,21 @@
 import { randomUUID } from "node:crypto";
 
+interface questionProps {
+  title: string;
+  content: string;
+  authorId: string;
+}
+
 export class Question {
   public id: string;
   public title: string;
   public content: string;
+  public authorId: string;
 
-  constructor(title: string, content: string, id?: string) {
-    this.title = title;
-    this.content = content;
+  constructor(props: questionProps, id?: string) {
+    this.title = props.title;
+    this.content = props.content;
+    this.authorId = props.authorId;
     this.id = id ?? randomUUID();
   }
 }

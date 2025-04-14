2b67565eb0a60bfffc98218773ff959829398154
Richard Lirio <richardlirio@hotmail.com>
2025-04-14 09:31:34 -0300

    feat: send notification on subscriber

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,4 +1,5 @@
 import { PaginationParams } from "@/core/repositories/pagination-params";
+import { DomainEvents } from "@/core/events/domain-events";
 import { AnswersRepository } from "@/domain/forum/application/repositories/answer-repository";
 import { Answer } from "@/domain/forum/enterprise/entities/answer";
 import { AnswerAttachmentsRepository } from "@/domain/forum/application/repositories/answer-attachments-repository";
@@ -24,6 +25,8 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id);
 
     this.items[itemIndex] = answer;
+
+    DomainEvents.dispatchEventsForAggregate(answer.id);
   }
 
   async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
@@ -36,6 +39,8 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async create(answer: Answer) {
     this.items.push(answer);
+
+    DomainEvents.dispatchEventsForAggregate(answer.id);
   }
 
   async delete(answer: Answer): Promise<void> {
